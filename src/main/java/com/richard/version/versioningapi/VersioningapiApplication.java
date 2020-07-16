ea2de710fd91afcf891a4ae2037a510ace979a85
richardpaull <richardj.paull@hotmail.co.uk>
2020-07-16 13:02:46 +0100

    First Commit with Build plugin

---
+++ src/main/java/com/richard/version/versioningapi/VersioningapiApplication.java
@@ -0,0 +1,13 @@
+package com.richard.version.versioningapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersioningapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/richard/version/versioningapi/VersioningapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richard.version.versioningapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
